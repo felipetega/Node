f6df0792159f3b070d25ddd05cf2c82931ff38a5
felipetega <felipe.crema.ribeiro@gmail.com>
2023-09-20 19:54:42 -0300

    ...

--- ecommerce-app-2023-main/config/db.js
+++ ecommerce-app-2023-main/config/db.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 import colors from "colors";
+mongoose.set('strictQuery', false);
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
