f8d2cedcfc2f9df0eb4aaaebce3c355030578b82
felipetega <felipe.crema.ribeiro@gmail.com>
2023-10-04 00:00:06 -0300

    route id in progress

---
+++ client-nextjs/app/products/[id]/page.js
@@ -0,0 +1,38 @@
+'use client'
+import { useEffect, useState } from 'react';
+
+export default function Page({ params }) {
+  const [product, setProduct] = useState(null);
+  console.log(params.id)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/products/find/${params.id}`);
+        const data = await response.json();
+        console.log(data)
+        setProduct(data);
+      } catch (error) {
+        console.error('Error fetching product:', error);
+      }
+    };
+
+    fetchProduct();
+  }, [params.id]);
+
+  console.log(product)
+
+  return (
+    <div>
+      <div>My Post: {params.id}</div>
+      {product && (
+        <div>
+          <h2>Product Details</h2>
+          <p>Name: {product.name}</p>
+          <p>Description: {product.description}</p>
+          {/* Adicione outros detalhes do produto conforme necessário */}
+        </div>
+      )}
+    </div>
+  );
+}
--- client-nextjs/components/Products.js
+++ client-nextjs/components/Products.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link'; // Importe o Link do Next.js
 
 const Products = ({ products }) => {
   const [selectedOption, setSelectedOption] = useState('default');
@@ -38,43 +39,37 @@ const Products = ({ products }) => {
             <li><a onClick={handleSelectChange}>Menor preço</a></li>
           </ul>
         </div>
-
-        {/* <div className="dropdown dropdown-bottom mx-2">
-          <label tabIndex={0} className="btn m-1">Filtrar por categoria</label>
-          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a onClick={handleCategoryChange}>Todos</a></li>
-            {allCategories.map((category, index) => (
-              <li key={index}><a onClick={handleCategoryChange}>{category}</a></li>
-            ))}
-          </ul>
-        </div> */}
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
         {sortedProducts.map(product => (
           <div key={product._id} className="card max-w-md bg-base-100 shadow-xl mb-4 mx-4">
-            <div style={{ position: 'relative', height: '300px' }}>
-              <Image
-                src={`/${product.img}`}
-                alt={product.title}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <div className="card-body p-4">
-              <div className="flex justify-between">
-                <h2 className="card-title text-2xl mb-4">
-                  {product.title}
-                </h2>
-                <div className="flex flex-wrap">
-                  {product.categories.map((category, index) => (
-                    <div key={index} className="badge badge-outline mr-2 mb-2">{category}</div>
-                  ))}
+            <Link href={`/products/${product._id}`}> {/* Adicione o Link aqui */}
+              <a>
+                <div style={{ position: 'relative', height: '300px' }}>
+                  <Image
+                    src={`/${product.img}`}
+                    alt={product.title}
+                    layout="fill"
+                    objectFit="cover"
+                  />
                 </div>
-              </div>
-              <p>{product._id}</p>
-              <p>{product.price}</p>
-            </div>
+                <div className="card-body p-4">
+                  <div className="flex justify-between">
+                    <h2 className="card-title text-2xl mb-4">
+                      {product.title}
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {product.categories.map((category, index) => (
+                        <div key={index} className="badge badge-outline mr-2 mb-2">{category}</div>
+                      ))}
+                    </div>
+                  </div>
+                  <p>{product._id}</p>
+                  <p>{product.price}</p>
+                </div>
+              </a>
+            </Link>
           </div>
         ))}
       </div>
